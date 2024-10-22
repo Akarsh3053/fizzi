7ec1a2d69604699c32d7421120a394a2c3912a6d
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-10-22 21:18:16 +0200

    add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+
+type Props = {}
+
+function Header({ }: Props) {
+    return (
+        <header className="flex justify-center py-4 -mb-28">
+            Header
+        </header>
+    )
+}
+
+export default Header
