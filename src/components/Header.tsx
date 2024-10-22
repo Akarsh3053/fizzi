b3c7042b985eede2019722563f8cb8ec077a7aba
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-10-22 15:18:16 +0200

    add hero slice

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
+import { FizziLogo } from "@/components/FizziLogo"
 
 type Props = {}
 
 function Header({ }: Props) {
     return (
         <header className="flex justify-center py-4 -mb-28">
-            Header
+            <FizziLogo className="h-20 z-10 cursor-pointer text-sky-800" />
         </header>
     )
 }
