3b6f9e433ebc96f95e3ed3bc052612e6bf93fffb
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-11-01 13:18:16 +0200

    hero animated

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,8 +1,15 @@
+'use client'
+
 import { Bounded } from "@/components/Bounded";
 import Button from "@/components/Button";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import { asText, Content } from "@prismicio/client";
-import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
+import { PrismicNextImage } from "@prismicio/next";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { TextSplitter } from "./TextSplitter";
+
+gsap.registerPlugin(useGSAP)
 
 /**
  * Props for `Hero`.
@@ -12,17 +19,53 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
 /**
  * Component for "Hero" Slices.
  */
+
+
 const Hero = ({ slice }: HeroProps): JSX.Element => {
+
+  useGSAP(() => {
+    const introTl = gsap.timeline()
+
+    introTl
+      .set(".hero", { opacity: 1 })
+      .from(".hero-header-word", {
+        scale: 3,
+        opacity: 0,
+        ease: "power4.in",
+        delay: 0.3,
+        stagger: 1,
+      })
+      .from(
+        ".hero-subheading",
+        {
+          opacity: 0,
+          y: 30,
+        },
+        "+=.8",
+      )
+      .from(".hero-body", {
+        opacity: 0,
+        y: 10,
+      })
+      .from(".hero-button", {
+        opacity: 0,
+        y: 10,
+        duration: 0.6,
+      });
+  })
+
+
   return (
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="hero opacity-0"
     >
       <div className="grid">
         <div className="grid h-screen place-items-center">
           <div className="grid auto-rows-min place-items-center text-center">
             <h1 className="hero-header lg:text-[13rem] text-7xl font-black uppercase leading-[.8] text-orange-500 md:text-[9rem]">
-              {asText(slice.primary.heading)}
+              <TextSplitter text={asText(slice.primary.heading)} wordDisplayStyle="block" className="hero-header-word" />
             </h1>
             <div className="hero-subheading mt-12 text-5xl font-semibold text-sky-950 lg:text-6xl">
               <PrismicRichText field={slice.primary.subheading} />
@@ -30,7 +73,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
             <div className="hero-body text-2xl font-normal text-sky-950">
               <PrismicRichText field={slice.primary.body} />
             </div>
-            <Button buttonLink={slice.primary.button_link} buttonText={slice.primary.button_text} className="hero-button mt-12" />
+            <Button className="hero-button mt-12" buttonLink={slice.primary.button_link} buttonText={slice.primary.button_text} />
           </div>
         </div>
 
@@ -41,7 +84,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
           />
           <div>
             <h2 className="text-side-heading text-balance text-6xl font-black uppercase text-sky-950 lg:text-8xl">
-              <PrismicRichText field={slice.primary.second_heading} />
+              <TextSplitter text={asText(slice.primary.second_heading)} />
             </h2>
             <div className="text-side-body mt-4 max-w-xl text-balance text-xl font-normal text-sky-950">
               <PrismicRichText field={slice.primary.second_body} />
