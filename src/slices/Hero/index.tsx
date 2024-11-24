a6d49ac5f08342f77dd58ec3c8817a5b28aeddff
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-11-24 15:20:16 +0200

    add the fizz

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -11,6 +11,7 @@ import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import { TextSplitter } from "@/components/TextSplitter";
 import { View } from "@react-three/drei";
 import Scene from "./Scene";
+import { Bubbles } from "./Bubbles";
 
 gsap.registerPlugin(useGSAP, ScrollTrigger)
 
@@ -101,6 +102,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
 
       <View className="hero-scene pointer-events-none sticky top-0 z-50 -mt-[100vh] hidden h-screen w-screen md:block">
         <Scene />
+        <Bubbles count={300} speed={2} repeat={true} />
       </View>
 
       <div className="grid">
