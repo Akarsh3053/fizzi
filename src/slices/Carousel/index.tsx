59b16c8fe89b7886ee38c117773b4116b667fd25
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-12-09 13:30:16 +0200

    add base template

--- src/slices/Carousel/index.tsx
+++ src/slices/Carousel/index.tsx
@@ -1,5 +1,24 @@
+'use client'
+
+import { SodaCanProps } from "@/components/SodaCan";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+
+const FLAVORS: {
+  flavor: SodaCanProps["flavor"];
+  color: string;
+  name: string;
+}[] = [
+    { flavor: "blackCherry", color: "#710523", name: "Black Cherry" },
+    { flavor: "grape", color: "#572981", name: "Grape Goodness" },
+    { flavor: "lemonLime", color: "#164405", name: "Lemon Lime" },
+    {
+      flavor: "strawberryLemonade",
+      color: "#690B3D",
+      name: "Strawberry Lemonade",
+    },
+    { flavor: "watermelon", color: "#4B7002", name: "Watermelon Crush" },
+  ];
 
 /**
  * Props for `Carousel`.
@@ -14,8 +33,12 @@ const Carousel = ({ slice }: CarouselProps): JSX.Element => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="carousel relative grid h-screen grid-rows-[auto,4fr,auto] justify-center overflow-hidden bg-white py-12 text-white"
     >
-      Placeholder component for carousel (variation: {slice.variation}) Slices
+      <div className="background pointer-events-none absolute inset-0 bg-[#710523] opacity-50" />
+
+      <PrismicRichText field={slice.primary.heading} />
+      <PrismicRichText field={slice.primary.price_copy} />
     </section>
   );
 };
